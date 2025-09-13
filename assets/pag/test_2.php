0317af791de0b234aa267da7581e7597a34a27f3
branislavpaunovic <branislav.paunovic.bp@gmail.com>
2025-09-13 19:55:04 +0200

    Initial commit

---
+++ assets/pag/test_2.php
@@ -0,0 +1,18 @@
+<! DOCTYPE html>
+<HEAD>
+	<title> "Ovo je neki teskt"</title>
+</HEAD>
+<body>
+
+	<?php
+		echo 'KOnacno sam poceo da ucim PHP';
+	?>
+	
+	<h1>Ovo je neki teskt </H1>
+	
+	
+	<?php
+		echo 'Cao';
+	?>
+</BODY>
+</html>
